4f793a752b831a1f4be6b106d375777f9e70cb10
martin6724 <martin.michelleann@gmail.com>
2018-05-30 13:41:52 -0400

    bootstrapping url shortener app

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_03_115457) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "urls", force: :cascade do |t|
     t.string "originalurl"
     t.string "random_id"
